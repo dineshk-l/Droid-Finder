80deaa5cd5b1544e7aff0f95afa001a4b55cdf7d
KVXLIZ <70652179+KVXLIZ@users.noreply.github.com>
2021-04-30 17:10:34 +0200

    Now when you press the button it sends the location

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.myapplication;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.View;
@@ -9,19 +15,32 @@ import android.widget.TextView;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private double latitude;
+    private double longitude;
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(final Location location) {
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+        }
+    };
 
 
-
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LocationManager mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000,
+                20, mLocationListener);
         final TextView textview1 = (TextView) findViewById(R.id.txtHello);
         Button button1 = (Button) findViewById(R.id.btnGPS);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String messageToSend = "this is a message";
+                String messageToSend = ("Latitude: " + latitude + ", Longitude: " + longitude + "\n");
                 String number = "+31657792925";
 
                 SmsManager.getDefault().sendTextMessage(number, null, messageToSend, null,null);
